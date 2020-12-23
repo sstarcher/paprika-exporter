b4a1c3482f5bc6da88b5c9bf29c5f715071756f7
datapolitical <datapolitical@gmail.com>
2020-12-23 15:17:11 -0800

    add setup and update requirements

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r") as fh:
+    requirements = [line.strip() for line in fh]
+
+setuptools.setup(
+    name="paprika-exporter",
+    version="0.0.1",
+    author="Chris Nicholson",
+    author_email="datapolitical@gmail.com",
+    description="A simple exporter for the paprika app.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=requirements,
+)
