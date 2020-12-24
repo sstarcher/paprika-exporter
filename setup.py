0c441d41dd4dbfbfd0d0da0979b3a66bd6e2c043
datapolitical <datapolitical@gmail.com>
2020-12-23 22:02:18 -0800

    moving files

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="papexp",
-    version="0.0.4",
+    version="0.0.6",
     author="Chris Nicholson",
     author_email="datapolitical@gmail.com",
     description="A simple exporter for the paprika app.",
