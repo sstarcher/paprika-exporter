3c9c04ede82635a33a46d3d56f46439d66984a8b
Chris Nicholson <datapolitical@gmail.com>
2021-10-12 10:06:34 -0700

    fix setup requirements

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-with open("requirements.txt", "r") as fh:
-    requirements = [line.strip() for line in fh]
 
 setuptools.setup(
     name="papexp",
@@ -20,5 +18,9 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires=requirements,
+    install_requires=[
+        'requests>=2.25.0',     
+        'python-dotenv>=0.15.0',
+        'PyYAML>=5.4'
+    ]
 )
