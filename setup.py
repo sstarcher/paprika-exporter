ee1510d8d0ed1100dfc30202db17f8de2239f619
datapolitical <datapolitical@gmail.com>
2021-10-13 17:42:42 -0700

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="papexp",
-    version="0.4.0",
+    version="0.4.1",
     author="Chris Nicholson",
     author_email="datapolitical@gmail.com",
     description="A simple exporter for the paprika app.",
